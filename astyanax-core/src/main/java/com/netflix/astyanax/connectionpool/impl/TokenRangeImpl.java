0b2f25127d4607382c16481cf90e1e6f6a670d56
DavidHerzogTU-Berlin <david.herzog@campus.tu-berlin.de>
2014-11-11 18:58:51 +0100

    got rid of ImmutableList from google, because it causes problems

--- astyanax-core/src/main/java/com/netflix/astyanax/connectionpool/impl/TokenRangeImpl.java
+++ astyanax-core/src/main/java/com/netflix/astyanax/connectionpool/impl/TokenRangeImpl.java
@@ -16,9 +16,10 @@
 package com.netflix.astyanax.connectionpool.impl;
 
 import java.util.List;
-
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
+import java.util.Collections;
+import java.util.ArrayList;
+//import com.google.common.collect.ImmutableList;
+//import com.google.common.collect.Lists;
 import com.netflix.astyanax.connectionpool.TokenRange;
 
 /**
@@ -36,9 +37,9 @@ public class TokenRangeImpl implements TokenRange {
         this.startToken = startToken;
         this.endToken = endToken;
         if (endpoints != null)
-            this.endpoints = ImmutableList.copyOf(endpoints);
+            this.endpoints = Collections.unmodifiableList(endpoints);
         else 
-            this.endpoints = Lists.newArrayList();
+            this.endpoints = new ArrayList<String>();
     }
 
     @Override
